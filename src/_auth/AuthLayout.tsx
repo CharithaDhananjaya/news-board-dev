fcb1c8a8f167574ee44850b870d3a3f59696dbb8
CharithaDhananjaya <charitha.dhananjaya464@gmail.com>
2024-01-23 23:53:15 +0100

    ⚡️ Firebase Successfull, Test Done

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,4 +1,26 @@
+import { useEffect } from "react";
+import { FirebaseAuth } from "@/firebase.init";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
 function AuthLayout() {
+  const email = "testuser@newsboard.com";
+  const password = "abcd1234";
+
+  useEffect(() => {
+    signInWithEmailAndPassword(FirebaseAuth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log("AuthUser", user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        console.log("ERROR", errorCode, errorMessage);
+      });
+  });
   return <div>AuthLayout</div>;
 }
 
--- src/firebase.init.ts
+++ src/firebase.init.ts
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,12 +10,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.APIKEY,
-  authDomain: import.meta.env.AUTHDOMAIN,
-  projectId: import.meta.env.PROJECTID,
-  storageBucket: import.meta.env.STORAGEBUCKET,
-  messagingSenderId: import.meta.env.MESSAGINGSENDERID,
-  appId: import.meta.env.APPID,
+  apiKey: "AIzaSyDmeW6YfZr-9s4G4mKIDAP43huh1Q2O0-A",
+  authDomain: "newsboard-74aaf.firebaseapp.com",
+  projectId: "newsboard-74aaf",
+  storageBucket: "newsboard-74aaf.appspot.com",
+  messagingSenderId: "754466951047",
+  appId: "1:754466951047:web:cfb7da545c47eafe9e0d22",
 };
 
 // init firebase
@@ -24,6 +24,6 @@ initializeApp(firebaseConfig);
 // init firestore service
 const FirebaseAuth = getAuth();
 const FirestoreDB = getFirestore();
-const FirebaseAnalytics = getAnalytics();
+//const FirebaseAnalytics = getAnalytics();
 
-export { FirebaseAuth, FirestoreDB, FirebaseAnalytics };
+export { FirebaseAuth, FirestoreDB };
