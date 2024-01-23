06924a9f55ba7f72444441663fe1ea0a025db3a6
CharithaDhananjaya <charitha.dhananjaya464@gmail.com>
2024-01-23 23:11:21 +0100

    ⚡️ Config Setup

--- src/firebase.init.ts
+++ src/firebase.init.ts
@@ -10,13 +10,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCH73tbgrL98TnxNGPyLkAmbz4mP4_uwmQ",
-  authDomain: "web3dviewer-69f36.firebaseapp.com",
-  projectId: "web3dviewer-69f36",
-  storageBucket: "web3dviewer-69f36.appspot.com",
-  messagingSenderId: "896058904953",
-  appId: "1:896058904953:web:d28b5dc881491037225fa8",
-  measurementId: "G-4QHLFJCTLC",
+  apiKey: import.meta.env.APIKEY,
+  authDomain: import.meta.env.AUTHDOMAIN,
+  projectId: import.meta.env.PROJECTID,
+  storageBucket: import.meta.env.STORAGEBUCKET,
+  messagingSenderId: import.meta.env.MESSAGINGSENDERID,
+  appId: import.meta.env.APPID,
 };
 
 // init firebase
