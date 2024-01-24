0c53f67cfbb0b305a11ff2f388f956e2504d1721
CharithaDhananjaya <charitha.dhananjaya464@gmail.com>
2024-01-24 23:39:43 +0100

    ⚡️ Created: Signup form created, no styles.

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,5 +1,42 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+
+import { INewUser } from "@/types/index";
+
 function SignupForm() {
-  return <div>SignupForm</div>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<INewUser>();
+
+  const registerUser: SubmitHandler<INewUser> = (data) => {
+    console.log("New User", data);
+  };
+
+  console.log(watch("email"));
+
+  return (
+    <form onSubmit={handleSubmit(registerUser)}>
+      <Input {...register("firstName")} />
+      <Input {...register("lastName")} />
+      <Input
+        {...register("email", {
+          required: true,
+          pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/,
+        })}
+      />
+      {errors.email && <span>This field is required</span>}
+
+      <Input {...register("userName")} />
+      <Input type="password" {...register("password")} />
+
+      <Button type="submit">Register the User</Button>
+    </form>
+  );
 }
 
 export default SignupForm;
