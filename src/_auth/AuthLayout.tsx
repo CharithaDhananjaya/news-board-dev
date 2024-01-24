854543f3b98afd0ddf2a47d77d7e5b78481c11ab
CharithaDhananjaya <charitha.dhananjaya464@gmail.com>
2024-01-25 00:44:13 +0100

    ⚡️ Layout: AuthLayout Improved.

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -24,9 +24,21 @@ function AuthLayout() {
       });
   });
   return (
-    <div>
-      <Outlet />
-    </div>
+    <>
+      <section className="flex flex-col items-center justify-center flex-1 px-10 py-5 md:px-20 md:py-10">
+        <Outlet />
+      </section>
+      <div className="relative flex-col items-center justify-center flex-1 hidden object-cover w-1/2 h-screen py-10 bg-no-repeat bg-sky-900 md:flex">
+        <div>
+          <span className="text-5xl font-medium text-center text-white">
+            NewsBoard
+          </span>
+        </div>
+        <div className="absolute bottom-0 p-2 text-sm font-light text-center text-white md:pb-4">
+          Created by Charitha Dhananjaya
+        </div>
+      </div>
+    </>
   );
 }
 
