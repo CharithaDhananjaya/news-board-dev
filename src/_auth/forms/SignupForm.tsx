0fdd19dfe74f0e12f2749600558605876d7098e6
CharithaDhananjaya <charitha.dhananjaya464@gmail.com>
2024-01-25 00:01:19 +0100

    🚧 Uncompleted: Form Multipled Feild Error Not Fixed.

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
 import Button from "@/components/Button";
@@ -9,7 +10,8 @@ function SignupForm() {
   const {
     register,
     handleSubmit,
-    watch,
+    setError,
+    setValue,
     formState: { errors },
   } = useForm<INewUser>();
 
@@ -17,23 +19,65 @@ function SignupForm() {
     console.log("New User", data);
   };
 
-  console.log(watch("email"));
+  useEffect(() => {
+    setError("firstName", { types: { required: "First Name is Required." } });
+    setError("lastName", { types: { required: "Last Name is Required." } });
+    setError("email", {
+      types: {
+        required: "Email is Required.",
+        pattern: "Enter Correct Email.",
+      },
+    });
+    setError("userName", { types: { required: "Username is Required." } });
+    setError("password", {
+      types: {
+        required: "Password is Required",
+        minLength:
+          "Password atleast 8 Characters, One letter, One Number and one Special Charactor.",
+      },
+    });
+  }, [setError]);
 
   return (
     <form onSubmit={handleSubmit(registerUser)}>
-      <Input {...register("firstName")} />
-      <Input {...register("lastName")} />
+      <Input {...register("firstName", { required: true })} />
+      {errors.firstName && errors.firstName.types && (
+        <p>{errors.firstName.types.required}</p>
+      )}
+      <Input {...register("lastName", { required: true })} />
+      {errors.lastName && errors.lastName.types && (
+        <p>{errors.lastName.types.required}</p>
+      )}
       <Input
         {...register("email", {
           required: true,
           pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/,
         })}
       />
-      {errors.email && <span>This field is required</span>}
-
-      <Input {...register("userName")} />
-      <Input type="password" {...register("password")} />
-
+      {errors.email && errors.email.types && (
+        <p>{errors.email.types.required}</p>
+      )}
+      {errors.email && errors.email.types && (
+        <p>{errors.email.types.pattern}</p>
+      )}
+      <Input {...register("userName", { required: true })} />
+      {errors.userName && errors.userName.types && (
+        <p>{errors.userName.types.required}</p>
+      )}
+      <Input
+        type="password"
+        {...register("password", {
+          required: true,
+          pattern:
+            /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+        })}
+      />
+      {errors.password && errors.password.types && (
+        <p>{errors.password.types.required}</p>
+      )}
+      {errors.password && errors.password.types && (
+        <p>{errors.password.types.pattern}</p>
+      )}
       <Button type="submit">Register the User</Button>
     </form>
   );
