db5b87c07bcc89c08639c8f06fa15611081542ac
CharithaDhananjaya <charitha.dhananjaya464@gmail.com>
2024-01-25 11:48:46 +0100

    refactor: removed the test auth code

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,28 +1,6 @@
-import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
-import { FirebaseAuth } from "@/firebase.init";
-import { signInWithEmailAndPassword } from "firebase/auth";
-
 function AuthLayout() {
-  const email = "testuser@newsboard.com";
-  const password = "abcd1234";
-
-  useEffect(() => {
-    signInWithEmailAndPassword(FirebaseAuth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log("AuthUser", user);
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-
-        console.log("ERROR", errorCode, errorMessage);
-      });
-  });
   return (
     <>
       <section className="flex flex-col items-center justify-center flex-1 px-10 py-5 md:px-20 md:py-10">
